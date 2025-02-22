aba720144267516aedf2b30b3d4419e55181a7c1
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-02-22 12:43:26 +0530

    add call bind

--- practice2/practice.js
+++ practice2/practice.js
@@ -1,28 +1,12 @@
-// const setUser = (userName) => {
-//   this.userName = userName;
-// };
-
-// const createUser = (userName, email, password) => {
-//   this.email = email;
-//   this.password = password;
-//   //   this.userName = userName;
-// };
-
-// const firstUser = createUser("Ram", "ram@gmail.com", "123");
-
-// console.log(firstUser);
-
 function setUser(userName) {
   this.userName = userName;
-  console.log(userName);
-}
-
-function createUser(userName, email, password) {
-  setUser.call(this, userName);
   this.email = email;
-  this.password = password;
+
+  console.log(userName);
+
+  console.log(email);
 }
 
-const firstUser = new createUser("Ram", "ram@gmail.com", "123");
+setUser.prototype.email = "ram"
 
-console.log(firstUser);
+console.log(setUser());
